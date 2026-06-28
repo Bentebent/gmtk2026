499749c3e65640cdc3d69803b625b399c8cc76ad
unknown <henriksson.tim@gmail.com>
2026-06-28 22:38:53 +0200

    feat: ray origin

--- compositor/RayMarchingCompositor.cs
+++ compositor/RayMarchingCompositor.cs
@@ -1,98 +1,118 @@
-﻿using System;
+using System;
 using Godot;
 using System.Collections.Generic;
 
 namespace GMTK2026.compositor {
-    [Tool]
-    [GlobalClass]
-    public partial class RayMarchingCompositor : CompositorEffect {
-        private RenderingDevice _renderingDevice;
-        private Rid _shader;
-        private Rid _computePipeline;
-        
-        public RayMarchingCompositor() {
-            EffectCallbackType = EffectCallbackTypeEnum.PostOpaque;
-            
-            RenderingServer.CallOnRenderThread(Callable.From(_Init));
-        }
+	[Tool]
+	[GlobalClass]
+	public partial class RayMarchingCompositor : CompositorEffect {
+		private RenderingDevice _renderingDevice;
+		private Rid _shader;
+		private Rid _computePipeline;
+		
+		public RayMarchingCompositor() {
+			EffectCallbackType = EffectCallbackTypeEnum.PostOpaque;
+			
+			RenderingServer.CallOnRenderThread(Callable.From(Initialize));
+		}
 
-        public void _Init() {
-            _renderingDevice = RenderingServer.GetRenderingDevice();
-            if (_renderingDevice == null) {
-                GD.PrintErr("No rendering device found!");
-                return;
-            }
+		public override void _Notification(int what) {
+			if (what != NotificationPredelete) {
+				return;
+			}
 
-            var spirv = GD.Load<RDShaderFile>("res://shaders/ray_marching.glsl").GetSpirV();
-            _shader = _renderingDevice.ShaderCreateFromSpirV(spirv);
+			if (_shader.IsValid) {
+				_renderingDevice.FreeRid(_shader);
+			}
+		}
 
-            if (!_shader.IsValid) {
-                GD.PrintErr("Failed to create shader!");
-                return;
-            }
-            
-            _computePipeline = _renderingDevice.ComputePipelineCreate(_shader);
-            GD.Print("Compute pipeline created!");
-        }
+		private void Initialize() {
+			_renderingDevice = RenderingServer.GetRenderingDevice();
+			if (_renderingDevice == null) {
+				GD.PrintErr("No rendering device found!");
+				return;
+			}
 
-        public override void _Notification(int what) {
-            if (what == NotificationPostinitialize) {
-                if (_shader.IsValid) {
-                    _renderingDevice.FreeRid(_shader);
-                }
-            }
-        }
+			var spirv = GD.Load<RDShaderFile>("res://shaders/ray_marching.glsl").GetSpirV();
+			_shader = _renderingDevice.ShaderCreateFromSpirV(spirv);
 
-        public override void _RenderCallback(int effectCallbackType, RenderData renderData) {
-            if (_renderingDevice == null || !_computePipeline.IsValid) {
-                return;
-            }
+			if (!_shader.IsValid) {
+				GD.PrintErr("Failed to create shader!");
+				return;
+			}
+			
+			_computePipeline = _renderingDevice.ComputePipelineCreate(_shader);
+			GD.Print("Compute pipeline created!");
+		}
 
-            var renderSceneBuffers = (RenderSceneBuffersRD)renderData.GetRenderSceneBuffers();
-            if (renderSceneBuffers == null) {
-                return;
-            }
-            
-            var size = renderSceneBuffers.GetInternalSize();
-            if (size.LengthSquared() == 0) {
-                return;
-            }
+		public override void _RenderCallback(int effectCallbackType, RenderData renderData) {
+			if (_renderingDevice == null || !_computePipeline.IsValid) {
+				return;
+			}
 
-            // Ceil to closest int
-            // Magic number (8) needs to match compute shader
-            var xGroups = (size.X - 1) / 8 + 1;
-            var yGroups = (size.Y - 1) / 8 + 1;
-            int zGroups = 1;
+			var renderSceneBuffers = (RenderSceneBuffersRD)renderData.GetRenderSceneBuffers();
+			if (renderSceneBuffers == null) {
+				return;
+			}
+			
+			var size = renderSceneBuffers.GetInternalSize();
+			if (size.LengthSquared() == 0) {
+				return;
+			}
 
-            float[] pushConstants = [
-                size.X,
-                size.Y,
-                0.0f,
-                0.0f
-            ];
+			// Ceil to closest int
+			// Magic number (8) needs to match compute shader
+			var xGroups = (size.X - 1) / 8 + 1;
+			var yGroups = (size.Y - 1) / 8 + 1;
+			int zGroups = 1;
 
-            var pushConstantBytes = new List<byte>();
-            Array.ForEach(pushConstants, x => { pushConstantBytes.AddRange(BitConverter.GetBytes(x)); });
-            
-            
-            var viewCount = (int)renderSceneBuffers.GetViewCount();
-            for (uint i = 0; i < viewCount; i++) {
-                var inputImage = renderSceneBuffers.GetColorLayer(i);
-                
-                var uniform = new RDUniform();
-                uniform.UniformType = RenderingDevice.UniformType.Image;
-                uniform.Binding = 0;
-                uniform.AddId(inputImage);
-                
-                var uniformSet = UniformSetCacheRD.GetCache(_shader, 0, new Godot.Collections.Array<RDUniform>(){uniform});
+			var camOrigin = new Vector4(
+				renderData.GetRenderSceneData().GetCamTransform().Origin.X,
+				renderData.GetRenderSceneData().GetCamTransform().Origin.Y,
+				renderData.GetRenderSceneData().GetCamTransform().Origin.Z,
+				1.0f
+			);
 
-                var computeList = _renderingDevice.ComputeListBegin();
-                _renderingDevice.ComputeListBindComputePipeline(computeList, _computePipeline);
-                _renderingDevice.ComputeListBindUniformSet(computeList, uniformSet, 0);
-                _renderingDevice.ComputeListSetPushConstant(computeList, pushConstantBytes.ToArray(), (uint)pushConstantBytes.Count);
-                _renderingDevice.ComputeListDispatch(computeList, (uint)xGroups, (uint)yGroups, (uint)zGroups);
-                _renderingDevice.ComputeListEnd();
-            }
-        }
-    }
+			var camToWorld = (
+				renderData.GetRenderSceneData().GetCamProjection() * 
+				new Projection(renderData.GetRenderSceneData().GetCamTransform())
+				).Inverse();
+			
+			Vector4[] pushConstants = [
+				new(size.X, size.Y, 0.0f,0.0f),
+				camToWorld.X,
+				camToWorld.Y,
+				camToWorld.Z,
+				camToWorld.W
+			];
+
+			var pushConstantBytes = new List<byte>();
+			Array.ForEach(pushConstants, v => {
+				pushConstantBytes.AddRange(BitConverter.GetBytes(v.X));
+				pushConstantBytes.AddRange(BitConverter.GetBytes(v.Y));
+				pushConstantBytes.AddRange(BitConverter.GetBytes(v.Z));
+				pushConstantBytes.AddRange(BitConverter.GetBytes(v.W));
+			});
+			
+			
+			var viewCount = (int)renderSceneBuffers.GetViewCount();
+			for (uint i = 0; i < viewCount; i++) {
+				var inputImage = renderSceneBuffers.GetColorLayer(i);
+				
+				var uniform = new RDUniform();
+				uniform.UniformType = RenderingDevice.UniformType.Image;
+				uniform.Binding = 0;
+				uniform.AddId(inputImage);
+				
+				var uniformSet = UniformSetCacheRD.GetCache(_shader, 0, new Godot.Collections.Array<RDUniform>(){uniform});
+
+				var computeList = _renderingDevice.ComputeListBegin();
+				_renderingDevice.ComputeListBindComputePipeline(computeList, _computePipeline);
+				_renderingDevice.ComputeListBindUniformSet(computeList, uniformSet, 0);
+				_renderingDevice.ComputeListSetPushConstant(computeList, pushConstantBytes.ToArray(), (uint)pushConstantBytes.Count);
+				_renderingDevice.ComputeListDispatch(computeList, (uint)xGroups, (uint)yGroups, (uint)zGroups);
+				_renderingDevice.ComputeListEnd();
+			}
+		}
+	}
 }
--- shaders/ray_marching.glsl
+++ shaders/ray_marching.glsl
@@ -6,12 +6,30 @@ layout (rgba16f, set = 0, binding = 0) uniform image2D color_image;
 
 layout(push_constant, std430) uniform PerFrame {
     vec2 raster_size;
-    vec2 reserved;
+    vec2 padding_0;
+    mat4 clip_to_world;
 } per_frame;
 
-void main() {
-    ivec2 uv = ivec2(gl_GlobalInvocationID.xy);
+vec3 get_ray_origin(vec2 ndc_xy, mat4 clip_to_world) {
+    //Godot uses reverse Z, hence the 1.0f
+    vec4 origin_clip = vec4(ndc_xy, 1.0f, 1.0f);
+    vec4 origin_world = clip_to_world * origin_clip;
     
-    vec4 color = imageLoad(color_image, uv);
-    imageStore(color_image, uv, color * vec4(1, 0, 0, 0.0f));
+    return origin_world.xyz / origin_world.w;
+}
+
+void main() {
+    ivec2 raster_coord = ivec2(gl_GlobalInvocationID.xy);
+   
+    if (raster_coord.x >= per_frame.raster_size.x || raster_coord.y >= per_frame.raster_size.y) {
+        return;
+    }
+    
+    //Linear remap raster to NDC
+    vec2 ndc_space_xy = 2.0 * (raster_coord / per_frame.raster_size) - 1.0;
+    vec3 ray_origin = get_ray_origin(ndc_space_xy, per_frame.clip_to_world); 
+    
+    vec4 color = imageLoad(color_image, raster_coord);
+
+    imageStore(color_image, raster_coord, color);
 }
