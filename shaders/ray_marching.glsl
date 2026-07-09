f72134b8b61d4441c1667ea7046eb40fb1f2ac14
unknown <henriksson.tim@gmail.com>
2026-07-09 15:59:39 +0200

    feat: tmax for ray

--- shaders/ray_marching.glsl
+++ shaders/ray_marching.glsl
@@ -17,39 +17,34 @@ layout (rgba16f, set = 1, binding = 0) uniform image2D color_image;
 struct Ray {
     vec3 origin;
     vec3 direction;
+    float t_max;
 };
-
-vec3 get_ray_origin(vec2 ndc_xy, mat4 clip_to_world) {
-    //Godot uses reverse Z, hence the 1.0f
-    vec4 origin_clip = vec4(ndc_xy, 1.0f, 1.0f);
-    vec4 origin_world = clip_to_world * origin_clip;
-    
-    return origin_world.xyz / origin_world.w;
-}
-
-vec3 get_ray_dir(vec2 ndc_xy, mat4 clip_to_world, vec3 ray_origin) {
-    vec4 end_clip = vec4(ndc_xy, 0.0f, 1.0f);
-    vec4 end_world = clip_to_world * end_clip;
-    vec3 ray_end = end_world.xyz / end_world.w; 
-
-    return normalize(ray_end - ray_origin);
-}
 
 Ray get_ray(vec2 ndc_xy, mat4 world_to_clip, mat4 clip_to_world) {
     //Godot uses reverse Z, hence the 1.0f Z
-    vec4 origin_clip = vec4(ndc_xy, 1.0f, 1.0f);
+    float z_n = 1.0f;
+    vec4 origin_clip = vec4(ndc_xy, z_n, 1.0f);
     vec4 origin_world = clip_to_world * origin_clip;
+    float n_fac = 1.0f / origin_world.w;
 
-    vec3 origin_homogenized = origin_world.xyz / origin_world.w;
+    vec3 origin_homogenized = origin_world.xyz * n_fac;
 
     vec3 mx = transpose(world_to_clip)[0].xyz;
     vec3 my = transpose(world_to_clip)[1].xyz;
+    vec3 mz = transpose(world_to_clip)[2].xyz;
     vec3 mw = transpose(world_to_clip)[3].xyz;
     vec3 u = fma(vec3(-ndc_xy.x), mw, mx);
     vec3 v = fma(vec3(-ndc_xy.y), mw, my);
-    vec3 ray_dir = normalize(cross(u, v));
+    vec3 d = cross(u, v);
+    float d_fac = inversesqrt(dot(d, d));
+    float den = dot(mz, d);
+    float t_max = -n_fac / (d_fac * den);
+
+    d_fac = (t_max > 0.0f) ? d_fac : -d_fac;
+    t_max = abs(t_max); 
+    vec3 ray_dir = d * d_fac;
     
-    return Ray(origin_homogenized, ray_dir);
+    return Ray(origin_homogenized, ray_dir, t_max);
 }
 
 float sdSphere(vec3 p, vec3 c, float r) {
@@ -59,9 +54,9 @@ float sdSphere(vec3 p, vec3 c, float r) {
 vec3 raymarch(in Ray ray) {
     float depth = 0.0f;
     
-    for (int i = 0; depth < 200 && i < 250000; i++) {
+    for (int i = 0; depth < ray.t_max && i < 250; i++) {
         vec3 p = ray.origin + ray.direction * depth;
-        float dist = sdSphere(p, vec3(0, 2, 0), 2);
+        float dist = sdSphere(p, vec3(0, 0, 0), 2);
         if (dist < 0.00001f) {
             return vec3(1, 0, 0);
         }
@@ -81,11 +76,7 @@ void main() {
     //Linear remap raster to NDC
     vec2 ndc_space_xy = 2.0 * (raster_coord / per_draw.raster_size) - 1.0;
     Ray ray = get_ray(ndc_space_xy, per_frame.world_to_clip, per_frame.clip_to_world);
-    //vec3 ray_origin = get_ray_origin(ndc_space_xy, per_frame.clip_to_world); 
-    //vec3 ray_dir = get_ray_dir(ndc_space_xy, per_frame.clip_to_world, ray_origin);
    
-    //vec3 adjusted_ray_dir = (ray_dir + 1.0f) / 2.0f;
-    
     //vec4 color = imageLoad(color_image, raster_coord);
 
     //imageStore(color_image, raster_coord, color);
